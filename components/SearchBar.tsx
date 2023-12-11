e3c6501137d7316e9c3e36c17dc981d148162fbc
Denis2982 <denisdenis2982@gmail.com>
2023-12-11 16:23:18 +0200

    added new components

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,20 @@
 
 import React, { useState } from 'react'
 import { SearchManufacturer } from './';
+import Image from 'next/image';
+
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
 
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('');
@@ -17,6 +31,7 @@ const SearchBar = () => {
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}
             />
+            <SearchButton />
         </div>
     </form>
   )
