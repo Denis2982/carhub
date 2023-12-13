292d99a60399f79887ec5dbef354d6eca5ba413a
Denis2982 <denisdenis2982@gmail.com>
2023-12-13 13:55:34 +0200

    fix error

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -53,7 +53,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                   value={query}
                   className='search-manufacturer__option'
                 >
-                  Create "{query}"
+                  {/* Create "{query}" */}
                 </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
